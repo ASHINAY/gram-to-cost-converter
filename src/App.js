2db53e5b8af2f36e2ff24ee86b5418e5e05ad84a
Nayak <anuja.nayak@capgemini.com>
2023-02-28 15:45:56 +0530

    Add new things

--- src/App.js
+++ src/App.js
@@ -16,20 +16,20 @@ function App() {
         <h style={{ fontWeight: '500' }}>BasicDetails</h>
         <div style={{ height: '1px', background: 'gray' }}></div>
         <div>Enter Price of 1 KG</div>
-        <input type='text' style={{ height: '40px' }}></input>
+        <input type='text' style={{ height: '30px',fontSize:'20px' }}></input>
         <div className="Container1" >Calculator</div>
         <div style={{ height: '1px', background: 'gray' }}></div>
         <div className="Container3">Choose One to Calculate</div>
-        <select name="calculationType" style={{ height: '40px' }} >
+        <select name="calculationType" style={{ height: '40px',fontSize:'20px' }} >
           <option value="Price">Price</option>
           <option value="gramsToCost">Grams</option>
           <option value="costToGrams"> KiloGrams</option>
         </select>
 
         <div className="Container2">Enter Price</div>
-        <input type='text' placeholder='Enter Price' style={{ height: '40px' }} ></input>
+        <input type='text' placeholder='Enter Price' style={{ height: '40px',fontSize:'20px' }} ></input>
 
-        <button onClick={onSearchBtnclick} style={{ backgroundColor: '#F16767', height: '40px', color: 'white', justifyContent: 'center', margin: '10px 0' }}>Calculate</button>
+        <button onClick={onSearchBtnclick} style={{ backgroundColor: '#F16767', height: '45px', color: 'white', justifyContent: 'center', margin: '10px 0',fontSize:'20px' }}>Calculate</button>
 
       </div>
       <div style={{ display: 'flex', justifyContent: 'center' }}>
