86b7732990e473cf61ccca5f363f434ab31d6106
Nayak <anuja.nayak@capgemini.com>
2023-03-01 14:49:41 +0530

    add hooks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [result, setResult] = useState(0);
 
   const onSearchBtnclick = () => {
-
-
+    
+    let calculatedResult = 0;
+    if (calculationType === 'Price') {
+      calculatedResult = input * price;
+    } else if (calculationType === 'gramsToCost') {
+      calculatedResult = (input / 1000) * price;
+    } else if (calculationType === 'costToGrams') {
+      calculatedResult = (input / price) * 1000;
+    }
+    setResult(calculatedResult);
   }
+
+  
   return (
     <div style={{ backgroundColor: "#fadfe3", height: '100vh' }}>
       <div className="App">
@@ -16,7 +27,7 @@ function App() {
         <h style={{ fontWeight: '500' }}>BasicDetails</h>
         <div style={{ height: '1px', background: 'gray' }}></div>
         <div>Enter Price of 1 KG</div>
-        <input type='text' style={{ height: '30px',fontSize:'20px' }}></input>
+        <input type='text' style={{ height: '45px',fontSize:'20px' }}></input>
         <div className="Container1" >Calculator</div>
         <div style={{ height: '1px', background: 'gray' }}></div>
         <div className="Container3">Choose One to Calculate</div>
@@ -35,6 +46,7 @@ function App() {
       <div style={{ display: 'flex', justifyContent: 'center' }}>
 
         <div className='subContainer'>
+         <div className='circle'>{result.toFixed(2)}</div>
           <span style={{ color: '#FFFFFF', fontWeight: '400', fontSize: '30px' }}>Answer there</span>
         </div>
       </div>
