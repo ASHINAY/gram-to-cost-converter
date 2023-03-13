486a63fa4e7234e9c2aa16f1a4eb7bcf5cdee915
ANUJANAY <anuja.nayak@capgemini.com>
2023-03-13 16:12:18 +0530

    getting error

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import { useState } from 'react';
 import './App.css';
+import CalculationComponent from './components/CalculationComponent';
 import Navbar from './components/Navbar';
 import ResultComponent from './components/ResultComponent';
+
 
 function App() {
   const [result, setResult] = useState(0);
@@ -37,38 +39,12 @@ function App() {
   return (
     <div style={{ backgroundColor: "#fadfe3", height: '100vh' }}>
       <Navbar projectName={projectName} />
-      <div className="Container">
-        <h style={{ fontWeight: '500' }}>BasicDetails</h>
-        <div style={{ height: '1px', background: 'gray' }}></div>
-        <div>Enter Price of 1 KG</div>
-        <input type='text'
-          style={{ height: '45px', fontSize: '20px' }}
-          onChange={(e) => { onChangePriceof1kg(e) }}></input>
-        <div className="Container1" >Calculator</div>
-        <div style={{ height: '1px', background: 'gray' }}></div>
-        <div className="Container3">Choose One to Calculate</div>
-        <select name="calculationType" style={{ height: '40px', fontSize: '20px' }}
-          value={calculationType}
-          onChange={(e) => setCalculationType(e.target.value)}>
-          <option value="Price">Price</option>
-          <option value="Grams">Grams</option>
-          <option value="KiloGrams"> KiloGrams</option>
-        </select>
-        <div className="Container2"> Enter {calculationType}</div>
-        <input type='text'
-          placeholder={`Enter ${calculationType == "Price"
-            ? "Price"
-            : calculationType == "Grams"
-              ? "Grams"
-              : "KiloGrams"
-            }`}
-          style={{ height: "40px", fontSize: "20px" }}
-          onChange={(e) => {
-            onChangePrice(e);
-          }}
-        ></input>
-        <button onClick={onCalculateBtnclick} style={{ backgroundColor: '#F16767', height: '45px', color: 'white', justifyContent: 'center', margin: '10px 0', fontSize: '20px' }}>Calculate</button>
-      </div>
+      <CalculationComponent  calculationType={setCalculationType}
+           onChangePriceof1kg={onChangePriceof1kg}
+           onChangePrice={onChangePrice}
+           onCalculateBtnclick={onCalculateBtnclick}
+           priceof1kg={priceof1kg}
+           price={price}/>
       {showResult &&
         <ResultComponent result={result} />
       }
